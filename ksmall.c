83a26312c77e52c8269fe2b7f5d8c540b5f2d1ee
Dhananjaya636 <53854006+Dhananjaya636@users.noreply.github.com>
2019-08-22 15:34:38 +0530

    Update ksmall.c

--- ksmall.c
+++ ksmall.c
@@ -28,7 +28,30 @@ int main()
 	printf("the smallest elemt is %d",a[k-1]);
 }
 	
-	
+output:
+
+ENTER THE NUMBER OF ELEMENTS TO INSERT INTO THE ARRAY:
+4
+
+ENTER THE ELEMENTS OF THE ARRAY:
+1
+2
+3
+4
+
+ENTER THE NUMBER OF MAX ELEMENTS:
+3
+
+SORTED ARRAY:
+
+1
+2
+3
+4
+MAXIMUM ELEMETS ARE:
+4
+3
+2
 
 
 
